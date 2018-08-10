557b14b3a068e3b9d0ba4d38296eee512e3ba935
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2018-08-10 14:21:16 +0800

    Change font to DejaVu Sans Mono 10

--- init.el
+++ init.el
@@ -23,7 +23,8 @@
 (column-number-mode t)
 (show-paren-mode t)
 
-(setq my-font "Source Code Pro-9:weight=book:width=regular")
+(setq my-font "DejaVu Sans Mono-10:weight=book:width=regular")
+
 (set-face-attribute 'default t :font my-font)
 (add-to-list 'default-frame-alist `(font . ,my-font))
 (add-to-list 'default-frame-alist '(height . 50))
