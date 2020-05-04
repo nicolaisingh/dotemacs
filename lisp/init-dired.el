b97d31c11d398b4d31bb9d879c2d2c15efa8e450
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-05-04 21:06:11 +0800

    dired: Update config

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -2,25 +2,33 @@
 
 ;;; Commentary:
 
-;; This contains init configuration for the package dired. 
+;; This contains init configuration for the package dired.
 
 ;;; Code:
 
 (require 'dired-x)
 
-(setq dired-listing-switches "-Alh")
+(setq dired-listing-switch-A "")
 
 (defun dired-set-listing-switches ()
   (setq dired-listing-switches
-	(concat "-lh" dired-listing-switch-A)))
+	(concat "--group-directories-first -lh" dired-listing-switch-A)))
+
+(defun message-other-files-state ()
+  (message "%s other files"
+	   (if (string-empty-p dired-listing-switch-A)
+	       "Hide"
+	     "Show")))
 
 (defun toggle-other-files ()
   (interactive)
   (if (string-empty-p dired-listing-switch-A)
       (setq dired-listing-switch-A "A")
     (setq dired-listing-switch-A ""))
+  (message-other-files-state)
   (dired-sort-other (dired-set-listing-switches)))
 
+(dired-set-listing-switches)
 (define-key dired-mode-map (kbd "C-c .") #'toggle-other-files)
 
 (provide 'init-dired)
