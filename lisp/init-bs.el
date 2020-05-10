28e16b9b4cafab6b9aad976fea50ff9cc4a1e9cd
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-05-10 17:14:16 +0800

    bs: Updates

--- lisp/init-bs.el
+++ lisp/init-bs.el
@@ -8,12 +8,19 @@
 
 (require 'bs)
 
-(setq bs-default-configuration "files-and-scratch")
+(setq bs-default-configuration "files-and-scratch"
+      bs-max-window-height 30
+      bs-minimal-buffer-name-column 25)
+
+;; bs-default-sort-name is not working
+;; (setq bs-default-sort-name "by filename")
+(setq bs--current-sort-function (assoc "by filename" bs-sort-functions))
 
 (global-set-key (kbd "C-x C-b") 'bs-show)
 (global-set-key (kbd "C-=") #'bs-cycle-next)
 (global-set-key (kbd "C-+") #'bs-cycle-previous)
 
+(add-hook 'bs-mode-hook #'hl-line-mode)
+
 (provide 'init-bs)
 ;;; init-bs.el ends here
-
