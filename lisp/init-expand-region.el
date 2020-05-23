fbd0a7699147ce3e6b966054787c5b73295aefae
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-05-23 11:09:29 +0800

    expand-region: More keybindings

--- lisp/init-expand-region.el
+++ lisp/init-expand-region.el
@@ -8,7 +8,10 @@
 
 (require 'expand-region)
 
-(global-set-key (kbd "C-c e r") 'er/expand-region)
+(global-set-key (kbd "C-c e r") #'er/expand-region)
+(global-set-key (kbd "C-c e i") #'er/mark-inside-pairs)
+(global-set-key (kbd "C-c e o") #'er/mark-outside-pairs)
+(global-set-key (kbd "C-c e s") #'er/mark-inside-quotes)
 
 (provide 'init-expand-region)
 ;;; init-expand-region.el ends here
