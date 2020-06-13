08d0e735035bd16f3fc8e31ddee4483449e4227b
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-06-13 17:35:57 +0800

    dired: Add binding for ediff-files

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -38,6 +38,24 @@
   (interactive "sFind-grep (grep regexp): ")
   (find-grep-dired (dired-current-directory) regexp))
 
+(defun restore-window-config ()
+  "Restore the previous window configuration before
+ediff-marked-files was ran."
+  (remove-hook 'ediff-after-quit-hook-internal #'ediff-restore-window-config)
+  (set-window-configuration my--previous-window-config)
+  (setq my--previous-window-config nil))
+
+(defun ediff-marked-files ()
+  "Run ediff-files on 2 marked files in dired.  Inspired by
+https://oremacs.com/2017/03/18/dired-ediff"
+  (interactive)
+  (let ((files (dired-get-marked-files)))
+    (if (not (= (length files) 2))
+        (error "Marked files != 2")
+      (setq my--previous-window-config (current-window-configuration))
+      (ediff-files (first files) (second files))
+      (add-hook 'ediff-after-quit-hook-internal #'restore-window-config))))
+
 (dired-set-listing-switches)
 
 (setq dired-hide-details-hide-symlink-targets nil
@@ -48,6 +66,7 @@
 (define-key dired-mode-map (kbd "C-c m .") #'toggle-other-files)
 (define-key dired-mode-map (kbd "C-c m f") #'current-directory-find-name-dired)
 (define-key dired-mode-map (kbd "C-c m g") #'current-directory-find-grep-dired)
+(define-key dired-mode-map (kbd "C-c m d") #'ediff-marked-files)
 
 (add-hook 'dired-mode-hook #'hl-line-mode)
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
