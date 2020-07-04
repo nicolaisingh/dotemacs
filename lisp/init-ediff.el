8ba2099409da2b66d6cf43187815bf2ba825b157
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-07-04 11:31:51 +0800

    ediff: Add ediff-last-2-kills
    Inspired by:
    https://gist.github.com/agzam/792ccef7eb57e9bcb27253be1bea16f3

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -22,5 +22,18 @@
 (add-hook 'ediff-before-setup-hook #'ediff-save-windows-config)
 (add-hook 'ediff-quit-hook #'ediff-restore-previous-window-config)
 
+(defun ediff-last-2-kills ()
+  "Run ediff on the last 2 kills."
+  (interactive)
+  (let ((text-a (current-kill 1 t))
+        (text-b (current-kill 0 t))
+        (a (generate-new-buffer (generate-new-buffer-name "diff-a")))
+        (b (generate-new-buffer (generate-new-buffer-name "diff-b"))))
+    (with-current-buffer a (insert text-a))
+    (with-current-buffer b (insert text-b))
+    (ediff-buffers a b)))
+
+(global-set-key (kbd "C-c d k") #'ediff-last-2-kills)
+
 (provide 'init-ediff)
 ;;; init-ediff.el ends here
