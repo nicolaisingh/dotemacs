501c1c7900211cf0906c7a8f64a8a515224c4844
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-07-18 11:18:32 +0800

    Update the initial scratch message

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -6,51 +6,31 @@
 
 ;;; Code:
 
-;; (setq my-scratch-message "#+TITLE: Scratch buffer
+(setq my-scratch-message ";;; This is the scratch buffer
 
-;; * Important Key Binding Conventions (from elisp manual)
-;; ** Reserved for users to customize
-;; *** C-c LETTER
-;; *** <F5>-<F9>
-;; ** Reserved for major modes
-;; *** C-c <CONTROL-CHAR>
-;; *** C-c C-#
-;; *** C-c { } < > : or ;
-;; ** Reserved for minor modes
-;; *** C-c any other symbol or punctuation
-;; ** Don't bind
-;; *** C-h following any prefix char (e.g. C-h C-c)
-;; *** C-x as this is for Emacs itself
-;; ")
-(setq my-scratch-message "# This is the scratch buffer
+;;; Key Binding Conventions:
 
-For icomplete:
-----
-C-M-i		minibuffer-force-complete
+;; For user bindings:	C-c LETTER, <F5>-<F9>
+;; For major modes:	C-c <CONTROL-CHAR>, C-c C-#, C-c { } < > : or ;
+;; For minor modes:	C-c <any other symbol or punctuation>
+;; Don't bind these:	C-h following any prefix char (e.g. C-h C-c) or C-x
 
-M-v		switch-to-completions
+;;; icomplete keys:
 
-M-n		next-history-element
-M-p		previous-history-element
-M-r		previous-matching-history-element
-M-s		next-matching-history-element\n
+;; C-M-i	minibuffer-force-complete
+;; M-v  	switch-to-completions
+;;
+;; M-n  	next-history-element
+;; M-p  	previous-history-element
+;; M-r  	previous-matching-history-element
+;; M-s  	next-matching-history-element\n
+;; M-v  	switch-to-completions
 
-Completion:
-----
-M-v		switch-to-completions
-
-Recursive minibuffer:
-----
-C-]		abort-recursive-edit
-
-Help:
-----
-C-h S		info-lookup-symbol
-C-h o		describe-symbol
+(my-find-init-file)
 ")
+
 (setq initial-scratch-message my-scratch-message)
 
-(setq initial-major-mode 'org-mode)
 (setq inhibit-startup-screen 1)
 (setq visible-bell 1)
 (setq ring-bell-function 'ignore)
