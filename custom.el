7a4df8e8256d9a925ee699e7efd59c798637a4d6
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2020-08-22 14:30:02 +0800

    Updates

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ibuffer-saved-filter-groups
-   (quote
-    (("nas"
+   '(("nas"
       ("Kotlin"
        (mode . kotlin-mode))
       ("Emacs Lisp"
@@ -17,21 +16,19 @@
       ("Dired"
        (mode . dired-mode))
       ("Shell"
-       (mode . shell-mode))))))
+       (mode . shell-mode)))))
  '(ibuffer-show-empty-filter-groups nil)
  '(kotlin-tab-width 4)
  '(mode-line-percent-position nil)
- '(org-agenda-files (quote ("~/org/notes.org" "~/org/personal.org")))
+ '(org-agenda-files '("~/org/notes.org" "~/org/personal.org"))
  '(package-selected-packages
-   (quote
-    (saveplace-pdf-view graphviz-dot-mode company-restclient restclient know-your-http-well multiple-cursors inf-clojure clojure-mode aggressive-indent tree-mode json-navigator prism find-file-in-repository dired-toggle diminish transpose-frame amx unfill readline-complete writeroom-mode counsel-projectile magit htmlize discover-my-major lsp-ui company-lsp lsp-mode flycheck flycheck-kotlin typing company edit-server js2-mode kotlin-mode smartparens json-mode origami browse-kill-ring ace-window which-key afternoon-theme plantuml-mode avy-zap centered-cursor-mode change-inner expand-region modalka smex ido-vertical-mode pdf-tools)))
- '(plantuml-default-exec-mode (quote jar))
+   '(saveplace-pdf-view graphviz-dot-mode company-restclient restclient know-your-http-well multiple-cursors inf-clojure clojure-mode aggressive-indent tree-mode json-navigator prism find-file-in-repository dired-toggle diminish transpose-frame amx unfill readline-complete writeroom-mode counsel-projectile magit htmlize discover-my-major lsp-ui company-lsp lsp-mode flycheck flycheck-kotlin typing company edit-server js2-mode kotlin-mode smartparens json-mode origami browse-kill-ring ace-window which-key afternoon-theme plantuml-mode avy-zap centered-cursor-mode change-inner expand-region modalka smex ido-vertical-mode pdf-tools))
+ '(plantuml-default-exec-mode 'jar)
  '(window-divider-default-right-width 3)
  '(window-divider-mode t)
  '(writeroom-extra-line-spacing 0.3)
  '(writeroom-global-effects
-   (quote
-    (writeroom-set-alpha writeroom-set-menu-bar-lines writeroom-set-tool-bar-lines writeroom-set-vertical-scroll-bars writeroom-set-bottom-divider-width)))
+   '(writeroom-set-alpha writeroom-set-menu-bar-lines writeroom-set-tool-bar-lines writeroom-set-vertical-scroll-bars writeroom-set-bottom-divider-width))
  '(x-stretch-cursor t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
