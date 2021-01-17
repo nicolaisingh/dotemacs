a241982fbf5cede19fa389e57b3f476ab6a8922b
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2021-01-17 17:15:11 +0800

    hippie-expand: Update init config

--- lisp/init-hippie-expand.el
+++ lisp/init-hippie-expand.el
@@ -1,4 +1,4 @@
-;;; init-hippie-expand.el --- Init configuration for hippie-expand
+;;; init-hippie-expand.el --- Init configuration
 
 ;;; Commentary:
 
@@ -6,19 +6,82 @@
 
 ;;; Code:
 
-(global-set-key (kbd "M-/") 'hippie-expand)
-
-(setq hippie-expand-try-functions-list
+;; try-functions for dabbrev expansion
+(setq my-he-try-functions-dabbrev
       '(try-expand-dabbrev
+        try-expand-dabbrev-visible
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
+        try-expand-all-abbrevs))
+
+;; try-functions for expanding lines
+(setq my-he-try-functions-line
+      '(try-expand-line
+        try-expand-line-all-buffers))
+
+;; try-functions for expanding filenames
+(setq my-he-try-functions-filename
+      '(try-complete-file-name-partially
+        try-complete-file-name))
+
+;; Combine all the above as the default behavior for `hippie-expand'
+(setq hippie-expand-try-functions-list (append
+                                        my-he-try-functions-dabbrev
+                                        my-he-try-functions-line
+                                        my-he-try-functions-filename))
+
+(fset 'hippie-expand-filename-first
+      (make-hippie-expand-function (append
+                                    my-he-try-functions-filename
+                                    my-he-try-functions-dabbrev
+                                    my-he-try-functions-line)
+                                   t))
+
+(fset 'hippie-expand-line-first
+      (make-hippie-expand-function (append
+                                    my-he-try-functions-line
+                                    my-he-try-functions-filename
+                                    my-he-try-functions-dabbrev)
+                                   t))
+
+(defun hippie-expand-disable-minor-modes ()
+  (interactive)
+  (hippie-expand-line-mode -1)
+  (hippie-expand-filename-mode -1))
+
+(define-minor-mode hippie-expand-filename-mode
+  "Modify hippie-expand to prioritize filename expansions."
+  :global t
+  :init-value nil
+  :lighter " HippieFilename"
+  :keymap
+  '(([?\M-/] . hippie-expand-filename-first)
+    ([?\C-g] . (lambda ()
+                 (interactive)
+                 (hippie-expand-disable-minor-modes)
+                 (keyboard-quit))))
+
+  (when hippie-expand-filename-mode
+    (hippie-expand-line-mode -1)))
+
+(define-minor-mode hippie-expand-line-mode
+  "Modify hippie-expand to prioritize line expansions."
+  :global t
+  :init-value nil
+  :lighter " HippieLine"
+  :keymap
+  '(([?\M-/] . hippie-expand-line-first)
+    ([?\C-g] . (lambda ()
+                 (interactive)
+                 (hippie-expand-disable-minor-modes)
+                 (keyboard-quit))))
+
+  (when hippie-expand-line-mode
+    (hippie-expand-filename-mode -1)))
+
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "C-c e f") #'hippie-expand-filename-mode)
+(global-set-key (kbd "C-c e l") #'hippie-expand-line-mode)
 
 (provide 'init-hippie-expand)
 ;;; init-hippie-expand.el ends here
