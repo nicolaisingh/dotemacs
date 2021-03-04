4ecba4885cd1f9b8fdf27775b7ffb8d4ee9ed2ba
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2021-03-04 21:28:11 +0800

    recentf: Use completing-read in recentf

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -9,10 +9,26 @@
 (add-hook 'after-init-hook #'recentf-mode)
 
 (setq recentf-max-menu-items 100
-      recentf-max-saved-items 100
-      recentf-menu-filter 'recentf-sort-directories-ascending)
+      recentf-max-saved-items 1000
+      recentf-auto-cleanup 60
+      recentf-menu-filter 'recentf-show-basenames-ascending)
 
-(global-set-key (kbd "C-x C-S-f") #'recentf-open-files)
+(defun recentf-open-files-completing-read ()
+  (interactive)
+  (let* ((recentf-menu (recentf-make-menu-items))
+         (menu-labels (mapcar (lambda (elt) (aref elt 0))
+                              recentf-menu))
+         (result (completing-read "Open recent: " menu-labels))
+         (selection-pos (cl-position result menu-labels :test 'equal))
+         (recentf-form (aref (nth selection-pos recentf-menu) 1)))
+    (cond ((functionp recentf-form)
+           ;; recentf uses lambdas to find-file an item
+           (funcall recentf-form))
+          ((eq 'customize-group (car recentf-form))
+           ;; The item "More..." needs to be eval'd
+           (eval recentf-form)))))
+
+(global-set-key (kbd "C-x C-S-f") #'recentf-open-files-completing-read)
 
 (provide 'init-recentf)
 ;;; init-recentf.el ends here
