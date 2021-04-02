895d8c3ccbc78b015495af59bb16f30b4da64951
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2021-04-02 10:04:37 +0800

    ediff: Kill temp buffers

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -6,6 +6,9 @@
 
 ;;; Code:
 
+(defvar ediff-temp-buffer-a "*diff-a*")
+(defvar ediff-temp-buffer-b "*diff-b*")
+
 (setq ediff-window-setup-function 'ediff-setup-windows-plain
       ediff-split-window-function 'split-window-horizontally)
 
@@ -14,7 +17,9 @@
 
 (defun ediff-restore-previous-window-config ()
   "Restore the previous window configuration stored in `ediff-previous-window-config'."
-  (set-window-configuration ediff-previous-window-config))
+  (set-window-configuration ediff-previous-window-config)
+  (kill-buffer ediff-temp-buffer-a)
+  (kill-buffer ediff-temp-buffer-b))
 
 (defun ediff-save-windows-config ()
   (setq ediff-previous-window-config (current-window-configuration)))
@@ -27,8 +32,8 @@
   (interactive)
   (let ((text-a (current-kill 1 t))
         (text-b (current-kill 0 t))
-        (a (generate-new-buffer (generate-new-buffer-name "diff-a")))
-        (b (generate-new-buffer (generate-new-buffer-name "diff-b"))))
+        (a (generate-new-buffer (generate-new-buffer-name ediff-temp-buffer-a)))
+        (b (generate-new-buffer (generate-new-buffer-name ediff-temp-buffer-b))))
     (with-current-buffer a (insert text-a))
     (with-current-buffer b (insert text-b))
     (ediff-buffers a b)))
