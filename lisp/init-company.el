6d270f64289a3c6c8320d5bceede6c41f3ce4da9
Nicolai Andrew Singh <nicolai.singh@gmail.com>
2021-04-26 20:35:02 +0800

    company: Updates

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -11,7 +11,8 @@
 (setq company-lighter nil
       company-minimum-prefix-length 2
       company-idle-delay 0.3
-      company-selection-wrap-around 1)
+      company-selection-wrap-around 1
+      company-require-match nil)
 
 (setq company-transformers '(company-sort-by-occurrence
                              company-sort-by-backend-importance))
@@ -19,11 +20,9 @@
 (add-to-list 'company-backends 'company-restclient)
 (add-to-list 'company-backends 'company-native-complete)
 
-(defun company-mode-my-custom-keys ()
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+(define-key company-active-map (kbd "C-n") #'company-select-next)
+(define-key company-active-map (kbd "C-p") #'company-select-previous)
 
-(add-hook 'company-mode-hook #'company-mode-my-custom-keys)
 (add-hook 'after-init-hook #'global-company-mode)
 
 (provide 'init-company)
