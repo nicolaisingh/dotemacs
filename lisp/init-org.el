05b03fd6646ddf94d77b499373ade87d00a34f76
Nicolai Singh <nicolaisingh@pm.me>
2023-08-18 22:20:04 +0800

    Updates

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -9,35 +9,33 @@
 (require 'org)
 
 (setq org-startup-indented 1
-      org-default-notes-file (concat org-directory "/notes.org")
+      org-default-notes-file "~/org/inbox.org"
       org-refile-targets '((nil :maxlevel . 3)
                            (org-agenda-files :maxlevel . 2))
-      org-complete-tags-always-offer-all-agenda-tags t)
+      org-complete-tags-always-offer-all-agenda-tags t
+	  org-refile-use-outline-path 'file
+	  org-outline-path-complete-in-steps nil)
 
-(setq org-capture-templates '(("c" "Capture something" entry
-                               (file+headline "~/org/gtd.org" "Inbox")
-                               "* %^{Topic|Capture} %U
-
-%?")
+(setq org-capture-templates '(("n" "Notes inbox" entry
+							   (file+headline "~/org/inbox.org" "Notes")
+							   "* %^{Content|Reminder}\n\n%U\n%?" :empty-lines 1)
+							  ("p" "Projects inbox" entry
+							   (file+headline "~/org/inbox.org" "Projects")
+							   "* %^{Content|Reminder}\n\n%U\n%?" :empty-lines 1)
+							  ("l" "Log" entry
+							   (file+olp+datetree "~/org/log.org")
+							   "* %?" :empty-lines 1)
                               ("D" "Distraction" item
-                               (file "~/org/distraction.org")
-                               "- %? %U")))
+							   (file "~/org/distraction.org")
+							   "- %? %U")))
 
-(setq org-tag-alist '(("@dev" . ?d)
-                      ("@idea" . ?i)
-                      ("@home" . ?h)
-                      ("@learn" . ?l)
-                      ("@project" . ?p)
-                      ("@task" . ?t)))
-
-(defun org-insert-filetags ()
-  (interactive)
-  (insert "#+FILETAGS: "))
+(setq org-tag-alist '(("@bib" . ?b)
+					  ("@project" . ?p)
+					  ("@task" . ?t)))
 
 (defun org-mode-my-custom-keys ()
   (define-key org-mode-map (kbd "C-c C--") #'org-ctrl-c-minus)
   (define-key org-mode-map (kbd "C-c C-8") #'org-ctrl-c-star)
-  (define-key org-mode-map (kbd "C-c o f") #'org-insert-filetags)
   (define-key org-mode-map (kbd "C-c C-SPC") #'org-table-blank-field))
 (add-hook 'org-mode-hook #'org-mode-my-custom-keys)
 
