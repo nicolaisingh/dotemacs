1fcdff1b84e66cf3afc4c1628e379f1190f5bebc
Nicolai Singh <nicolaisingh@pm.me>
2023-11-01 13:35:33 +0800

    Updates

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -10,6 +10,7 @@
 (require 'org-roam-dailies)
 
 (setq org-roam-directory "~/org/roam"
+      org-roam-db-location "~/org/org-roam.db"
       org-roam-completion-everywhere t
       org-roam-node-display-template (concat
                                       "${title:100} "
@@ -35,17 +36,12 @@
                                                      "#+title: ${title}\n")
          :empty-lines-before 1
          :unnarrowed t)
-        ("D" "default immediate" plain "%?" :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                               "#+title: ${title}\n")
-         :immediate-finish t
-         :empty-lines-before 1
-         :unnarrowed t)
         ("l" "literature" plain "%?"
          :target (file+head "literature/%<%Y%m%d%H%M%S>-${slug}.org"
                             "#+filetags: :@lit:\n#+title: ${title}")
          :empty-lines-before 1
          :unnarrowed t)
-        ("L" "literature index" plain "%?"
+        ("x" "index" plain "%?"
          :target (file+head "index/%<%Y%m%d%H%M%S>-${slug}.org"
                             "#+filetags: :@lit:index:\n#+title: ${title}\n#+author: %^{author}")
          :empty-lines-before 1
@@ -72,17 +68,25 @@
 
 (org-roam-db-autosync-mode)
 
+(defun org-roam-node-insert-immediate-finish ()
+  (interactive)
+  (let ((org-roam-capture-templates (mapcar (lambda (elt)
+                                              (append elt '(:immediate-finish t)))
+                                            org-roam-capture-templates)))
+    (call-interactively #'org-roam-node-insert)))
+
 (let ((map global-map))
   (define-key map (kbd "C-c n l") #'org-roam-buffer-toggle)
   (define-key map (kbd "C-c n f") #'org-roam-node-find)
   (define-key map (kbd "C-c n i") #'org-roam-node-insert)
+  (define-key map (kbd "C-c n I") #'org-roam-node-insert-immediate-finish)
   (define-key map (kbd "C-c n c") #'org-roam-capture)
   (define-key map (kbd "C-c n g") #'org-roam-graph)
   (define-key map (kbd "C-c n a") #'org-roam-alias-add)
   (define-key map (kbd "C-c n A") #'org-roam-alias-remove)
   (define-key map (kbd "C-c n t") #'org-roam-tag-add)
   (define-key map (kbd "C-c n T") #'org-roam-tag-remove)
-  (define-key map (kbd "C-c n I") #'org-id-get-create)
+  (define-key map (kbd "C-c n n") #'org-id-get-create)
   (define-key map (kbd "C-c n r") #'org-roam-ref-add)
   (define-key map (kbd "C-c n R") #'org-roam-ref-remove)
   (define-key map (kbd "C-c n x") #'org-roam-extract-subtree)
