bda7779cc1a37ff4d7f3fff78df81ae6efe83867
Nicolai Singh <nicolaisingh@pm.me>
2024-02-25 14:19:26 +0800

    updates

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,8 +6,32 @@
 
 ;;; Code:
 
-(elpy-enable)
 (setq python-indent-def-block-scale 1)
+;; (elpy-enable)
+
+(add-hook 'python-mode-hook #'eglot-ensure)
+
+(defun pyright-make-config (selection)
+  "Write pyrightconfig.json venv settings for a project."
+  (interactive (list (read-directory-name "Which venv to use: "
+                                          "~/.virtualenvs/"
+                                          "~/.virtualenvs/"
+                                          t)))
+  (let ((venv-dir "~/.virtualenvs")
+        (venv-name (car (last (split-string
+                               (expand-file-name selection)
+                               "/"
+                               t))))
+        (out-dir (vc-root-dir))
+        (out-filename "pyrightconfig.json"))
+    (if (not out-dir)
+        (message "Cannot find project root")
+      (with-temp-buffer
+
+        (insert (json-encode `(:venvPath ,venv-dir :venv ,venv-name)))
+        (json-pretty-print-buffer)
+        (write-file (concat out-dir out-filename))
+        (message "Wrote config to %s" (concat out-dir out-filename))))))
 
 (provide 'init-python)
 ;;; init-python.el ends here
