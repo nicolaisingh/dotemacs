7b56b23924b1a04d4e1a76e06d5a9d960fe7f30d
Nicolai Singh <nicolaisingh@pm.me>
2024-04-29 21:31:56 +0800

    updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotemacs / .emacs #
+
+My Emacs init file.
+
+Most of my configuration is stored in init.el.  Page navigation (`backward-page` and `forward-page`) and `imenu` are very helpful commands to jump around the file to edit and organize the code.
--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -sn $(pwd)/init.el ~/.emacs.d/init.el
 ln -sn $(pwd)/early-init.el ~/.emacs.d/early-init.el
 ln -sn $(pwd)/custom.el ~/.emacs.d/custom.el
 ln -sn $(pwd)/lisp ~/.emacs.d/lisp
+ln -sn $(pwd)/abbrev-defs.el ~/.emacs.d/abbrev-defs.el
 ln -sn $(pwd)/tree-sitter ~/.emacs.d/tree-sitter
 ln -sn $(pwd)/snippets ~/.emacs.d/snippets
 ln -sn $(pwd)/eshell/alias ~/.emacs.d/eshell/alias
